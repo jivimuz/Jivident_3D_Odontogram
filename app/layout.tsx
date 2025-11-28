84e3870febd14d29dffcc19cc2efaf3db3d16e92
jivimuz <jivirasgal@gmail.com>
2025-11-28 09:26:02 +0700

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
        <head>
+        <meta name="google-adsense-account" content="ca-pub-3791184145323279"/>
         <Script
           async
           strategy="afterInteractive"
